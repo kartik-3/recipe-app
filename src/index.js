00da2b8b023dad0b24d6bbc4ae464a236466ec12
Kartik Sehgal <kartiksehgal3@gmail.com>
2021-01-05 20:32:59 +0530

    1

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const App = () => {
+//     return (
+//         <h1>Recipes</h1>
+//     )
+// };
+
+// ReactDOM.render(<App />, document.querySelector("#react-root"));
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Demo from "./demo";
+
+ReactDOM.render(<Demo />, document.querySelector("#react-root"));
